c3c06f8fb9b50c9a4e538778c489dad7e2a14447
Kai Alcayde <kai.alcayde12@gmail.com>
2024-08-08 08:05:29 -0700

    currrent

--- Travel_App/backend.py
+++ Travel_App/backend.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, request
 # mlink to static
 
 # url_for('index') is for dynamic routes
+# dynamic handling is a little different (constantly changing)
 
 app = Flask(__name__)
 
@@ -13,6 +14,15 @@ app = Flask(__name__)
 @app.route("/") 
 def index(): 
     return render_template('index.html') 
+
+# serve static file through api endpoint
+# fancy way of saying doing a function when an http request is received
+# can be PUT, GET, etc
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
+
+
   
 # run the application 
 if __name__ == "__main__": 
